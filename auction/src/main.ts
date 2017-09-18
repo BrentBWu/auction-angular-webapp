49d35745332bd0376993fd605f317c860cd03e77
Arhdel <brentbwu@gmail.com>
2017-09-18 16:14:59 +1200

    update project

--- auction/src/main.ts
+++ auction/src/main.ts
@@ -1,12 +1,14 @@
+import './polyfills.ts';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from './app/app.module'; // 导入主模块
+import { environment } from './environments/environment'; // 导入环境配置
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode(); // 生产模式
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule) // 传入AppModule启动
   .catch(err => console.log(err));
